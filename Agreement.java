c70527108849f2243ad62a8dbbd339e86d32bf1f
elysee <elysee247@gmail.com>
2017-04-18 18:19:30 +1000

    test elysee's classes

---
+++ Agreement.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2608;
+
+/**
+ *
+ * @author elyse
+ */
+public class Agreement {
+    
+    private Member seeker;
+    private Member offerer;
+    private String status;
+    private String payAmt;
+    private String agreeDate;
+    private String uniqueNo;
+    private String createDay;
+    private String initiateBy;
+    private CarOffer offer;
+    private Double offerReceivable;
+    private Long toPin;
+    private Long fromPin;
+    private Long pUpFrom;
+    private Long pUpTo;
+    private String day;
+
+}
