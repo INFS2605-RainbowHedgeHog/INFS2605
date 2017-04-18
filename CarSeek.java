50405e33fec45c13a7b578081927b4192fff365b
GentleManan <manan.patel.1142@gmail.com>
2017-04-18 18:54:33 +1000

    Update CarSeek.java

--- CarSeek.java
+++ CarSeek.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -15,21 +14,4 @@ public class CarSeek {
     private Member ownerMember = new Member();
     
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Classes;
 
-/**
- *
- * @author manan
- */
-public class CarSeek {
-    
-    private Member ownerMember = new Member();
-    
-}
->>>>>>> d24eb603feb7356f53eb7e8dd8323fe0ce972857
