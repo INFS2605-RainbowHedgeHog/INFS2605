b099b4336557e424d698bbc089ea8efe6859a7e1
elyseeng <elyseeng247@gmail.com>
2017-04-18 19:45:50 +1000

    Add files via upload

--- UserOption.java
+++ UserOption.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package classes;
+package Classes;
 
 /**
  *
@@ -18,6 +18,94 @@ public class UserOption {
     protected Vector agreementList;
     protected String uniqueNo;
     protected Int flag;
-    protected string createDay;
+    protected String createDay;
+
+    //setters
+    public void setToPin(Long toPin) {
+        this.toPin = toPin;
+    }
+
+    public void setFromPin(Long fromPin) {
+        this.fromPin = fromPin;
+    }
+
+    public void setpUpFrom(Long pUpFrom) {
+        this.pUpFrom = pUpFrom;
+    }
+
+    public void setpUpTo(Long pUpTo) {
+        this.pUpTo = pUpTo;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setAgreementList(Vector agreementList) {
+        this.agreementList = agreementList;
+    }
+
+    public void setUniqueNo(String uniqueNo) {
+        this.uniqueNo = uniqueNo;
+    }
+
+    public void setFlag(Int flag) {
+        this.flag = flag;
+    }
+
+    public void setCreateDay(String createDay) {
+        this.createDay = createDay;
+    }
+
+    
+    //getters
+    public Long getToPin() {
+        return toPin;
+    }
+
+    public Long getFromPin() {
+        return fromPin;
+    }
+
+    public Long getpUpFrom() {
+        return pUpFrom;
+    }
+
+    public Long getpUpTo() {
+        return pUpTo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Vector getAgreementList() {
+        return agreementList;
+    }
+
+    public String getUniqueNo() {
+        return uniqueNo;
+    }
+
+    public Int getFlag() {
+        return flag;
+    }
+
+    public String getCreateDay() {
+        return createDay;
+    }
+
+    //constructor
+    public UserOption(Long toPin, Long fromPin, Long pUpFrom, Long pUpTo, String date, Vector agreementList, String uniqueNo, Int flag, String createDay) {
+        this.toPin = toPin;
+        this.fromPin = fromPin;
+        this.pUpFrom = pUpFrom;
+        this.pUpTo = pUpTo;
+        this.date = date;
+        this.agreementList = agreementList;
+        this.uniqueNo = uniqueNo;
+        this.flag = flag;
+        this.createDay = createDay;
+    }
     
 }
