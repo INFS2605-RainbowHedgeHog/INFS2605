8c32e6201d685eb50dbbb7209955a761f95819d0
GentleManan <manan.patel.1142@gmail.com>
2017-04-18 18:53:58 +1000

    Add files via upload

---
+++ CarSeek.java
@@ -0,0 +1,35 @@
+<<<<<<< HEAD
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author manan
+ */
+public class CarSeek {
+    
+    private Member ownerMember = new Member();
+    
+}
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author manan
+ */
+public class CarSeek {
+    
+    private Member ownerMember = new Member();
+    
+}
+>>>>>>> d24eb603feb7356f53eb7e8dd8323fe0ce972857
---
+++ Corporate.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author manan
+ */
+public class Corporate {
+    protected String ID;
+    protected String companyName;
+    protected String companyAddress;
+    protected long companyPhoneNum;
+    protected long companyCode;
+    private int customerType;
+    
+    
+            
+}
