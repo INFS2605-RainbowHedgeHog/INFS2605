7d30c31e3efe57f377482e2fc99baacff72a6ad3
GentleManan <manan.patel.1142@gmail.com>
2017-04-18 18:17:37 +1000

    more classes

---
+++ CarSeek.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author manan
+ */
+public class CarSeek {
+    
+    private Member ownerMember = new Member();
+    
+}
